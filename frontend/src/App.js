84576f3cd0e2908a8fa9887ff0d9dada75eb6711
zhengmianmian <zmianmian8@gmail.com>
2024-06-29 21:24:07 +1200

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      
+      <h1 className="bg-red-200">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+
+function Navbar() {
+  const unitStyle = "p-4 hover:bg-stone-700 hover:text-sky-400 transition duration-300 hover:cursor-pointer"
+  return (
+    <div className="sticky top-0 z-50">
+      <nav className="bg-sky-400 bg-border-gray-200">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-0">
+          <div className={unitStyle}>AppName</div>
+          <div className="flex">
+            <div className={unitStyle}>Sign in</div>
+            <div className={unitStyle}>Sign up</div>
+          </div>
+          
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
