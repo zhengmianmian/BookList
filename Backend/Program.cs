b1e9aee66d78c3c333e8bd5af45588a9bf0f1031
zhengmianmian <zmianmian8@gmail.com>
2024-07-09 15:22:58 +1200

    use .env in backend

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,14 +1,37 @@
+using System;
 using System.Text;
 using Backend.DB;
 using Backend.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Load the .env file
+Env.Load();
+
 // Add services to the container.
-builder.Services.Configure<DatabaseSettings>(
-    builder.Configuration.GetSection("SimpleAuthDatabase"));
+// builder.Services.Configure<DatabaseSettings>(
+//     builder.Configuration.GetSection("SimpleAuthDatabase"));
+
+// Read environment variables
+var dbConnectionString = Environment.GetEnvironmentVariable("SIMPLEAUTHDATABASE__CONNECTIONSTRING");
+Console.WriteLine("connection string: "+ dbConnectionString);
+var dbName = Environment.GetEnvironmentVariable("SIMPLEAUTHDATABASE__DATABASENAME");
+var dbUsersCollection = Environment.GetEnvironmentVariable("SIMPLEAUTHDATABASE__USERSCOLLECTION");
+var dbBooksCollection = Environment.GetEnvironmentVariable("SIMPLEAUTHDATABASE__BOOKSCOLLECTION");
+var jwtKey = Environment.GetEnvironmentVariable("SIMPLEAUTHDATABASE__JWTKEY");
+// Use the configuration in the application
+builder.Services.Configure<DatabaseSettings>(options =>
+{
+    options.ConnectionString = dbConnectionString;
+    options.DatabaseName = dbName;
+    options.CollectionName = dbUsersCollection;
+    options.BooksCollectionName = dbBooksCollection;
+    options.JwtKey = jwtKey;
+});
+
 builder.Services.AddSingleton<UsersService>();
 builder.Services.AddSingleton<BooksService>();
 
@@ -25,7 +48,7 @@ builder.Services.AddAuthentication(x =>
     x.SaveToken = true;
     x.TokenValidationParameters = new TokenValidationParameters{
         ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SimpleAuthDatabase:JwtKey"])),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
         ValidateIssuer = false,
         ValidateAudience = false
     };
@@ -43,18 +66,12 @@ builder.Services.AddCors(options =>
 });
 
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    // app.UseSwagger();
-    // app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
